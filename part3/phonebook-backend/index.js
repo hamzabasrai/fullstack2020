abb01523141c42e1a79e0abcd35e116b93962f0c
Hamza Basrai <hamza.basrai@gmail.com>
2020-04-18 22:12:11 -0400

    3.8 Updated morgan logging to show request body

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const morgan = require("morgan");
+morgan.token("body", (req, res) => JSON.stringify(req.body));
 
 const app = express();
 app.use(express.json());
-app.use(morgan("tiny"));
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
