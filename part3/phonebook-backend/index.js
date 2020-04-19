58f73df988b3a33a9b3e71ba5e7da715eab1584a
Hamza Basrai <hamza.basrai@gmail.com>
2020-04-18 21:26:45 -0400

    3.3 Add get endpoint for single perosn

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-let notes = [
+let persons = [
   {
     name: "Arto Hellas",
     number: "040-123456",
@@ -27,13 +27,24 @@ let notes = [
 
 app.get("/api/persons", (req, res) => {
   console.log("api/persons");
-  res.json(notes);
+  res.json(persons);
+});
+
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    return res.status(404).send({ error: "Person does not exist" });
+  }
 });
 
 app.get("/info", (req, res) => {
   const date = new Date();
   const html = `
-    <p>Phonebook has info for ${notes.length} people</p>
+    <p>Phonebook has info for ${persons.length} people</p>
     <p>${date.toDateString()} ${date.toTimeString()}</p>
   `;
 
