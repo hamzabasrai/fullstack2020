95806af94e3a243483e409b1a165c990e8da327e
Hamza Basrai <hamza.basrai@gmail.com>
2020-05-13 21:57:16 -0400

    7.8 Implemented useResource hook

--- part7/ultimate-hooks/src/App.js
+++ part7/ultimate-hooks/src/App.js
@@ -18,10 +18,14 @@ const useField = (type) => {
 const useResource = (baseUrl) => {
   const [resources, setResources] = useState([]);
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl).then((response) => setResources(response.data));
+  }, [baseUrl]);
 
   const create = (resource) => {
-    // ...
+    axios
+      .post(baseUrl, resource)
+      .then((response) => setResources(resources.concat(response.data)));
   };
 
   const service = {
